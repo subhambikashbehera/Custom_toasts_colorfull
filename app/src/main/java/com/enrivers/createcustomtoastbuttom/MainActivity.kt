115122f27a863bae84ef62ac81be5930028599dd
subham bikash behera <subhambikashbehera@gmail.com>
2022-12-08 17:44:55 +0530

    small changes

--- app/src/main/java/com/enrivers/createcustomtoastbuttom/MainActivity.kt
+++ app/src/main/java/com/enrivers/createcustomtoastbuttom/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-
+        supportActionBar?.hide()
 
         binding.button.setOnClickListener {
             successToast()
